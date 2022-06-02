92e666cb879ab67ca5aa6434e44785c8bb6fbc42
Andrea Favaretto <andrea.favaretto@meduse.design>
2022-06-02 14:14:19 +0200

    Stylized outline text

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
              alt="Robot Shit"/>
 
         <div class="relative flex flex-col justify-center items-center z-20">
-          <h1 class="mt-2 lg:mt-0 text-18 lg:text-21 font-bold leading-none text-center">
+          <h1 class="mt-2 lg:mt-0 text-18 lg:text-40 lg:leading-35 font-bold leading-none text-center">
             <span class="block" v-text="data.hero.title"/>
             <span class="block" v-text="data.event.where"/>
             <span class="block" v-text="data.event.when"/>
@@ -19,22 +19,31 @@
         </div>
       </section>
 
-      <section>
-        <h2 v-html="data.section1.title"/>
-        <img :src="data.section1.image.url"/>
-        <p v-html="data.section1.image.description"/>
+      <section class="relative flex items-center section-line">
+        <div class="grid grid-cols-12">
+          <div class="col-span-5 col-start-2">
+            <img :src="data.section1.image.url"/>
+            <p class="mt-4 font-secondary" v-html="data.section1.image.description"/>
+          </div>
+
+          <div class="absolute text-right overlay-text">
+            <h2 class="text-50 leading-64 font-bold uppercase outlined" v-html="data.section1.title"/>
+          </div>
+        </div>
       </section>
 
-      <section>
-        <h2>
-          <span v-html="data.section2.title.el1"/>
-          <span v-html="data.section2.title.el2"/>
-        </h2>
+      <section class="flex items-center section-line">
+        <div>
+          <h2 class="text-50 leading-64 font-bold">
+            <span v-html="data.section2.title.el1"/>
+            <span v-html="data.section2.title.el2"/>
+          </h2>
 
-        <p v-html="data.section2.description"/>
+          <p v-html="data.section2.description"/>
+        </div>
       </section>
 
-      <section class="bg-black text-white">
+      <section class="bg-black text-white section-line">
         <h2>
           <span v-html="data.section3.title.el1"/>
           <span v-html="data.section3.title.el2"/>
@@ -45,7 +54,7 @@
         <p v-html="data.section3.description"/>
       </section>
 
-      <section class="section-horizontal">
+      <section class="section-line section-horizontal">
         <div>
           <h2 v-html="data.sectionHorizontal['1'].title"/>
         </div>
@@ -115,5 +124,14 @@ body,
     @apply block absolute top-0 left-0 w-full h-full bg-black opacity-20 z-10;
     content: '';
   }
+}
+
+.section-line {
+  height: 706px;
+}
+
+.overlay-text {
+  width: 50%;
+  left: 40%;
 }
 </style>
