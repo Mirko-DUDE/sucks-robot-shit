452070d8bd2f393878b55cce3c2fa1285ce9a444
Andrea Favaretto <andrea.favaretto@meduse.design>
2022-06-08 09:05:10 +0200

    Reduced mobile logo size

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,19 @@
 <template>
   <header class="absolute top-0 left-0 w-full z-30 text-center">
-    <img class="inline-block relative" src="/images/logo.svg" alt="Robot Shit logo">
+    <img class="inline-block relative logo" src="/images/logo.svg" alt="Robot Shit logo">
   </header>
 </template>
 
 <style lang="postcss" scoped>
 header {
   padding-top: 62px;
+}
+
+.logo {
+  max-width: 272px;
+
+  @screen lg {
+    max-width: none;
+  }
 }
 </style>
