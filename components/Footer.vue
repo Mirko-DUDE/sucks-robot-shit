efb6277730a404da1d975f5cce0c046392910b2c
Andrea Favaretto <andrea.favaretto@meduse.design>
2022-06-08 19:25:51 +0200

    Fixed horizontal scrolling section height

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,31 +1,47 @@
 <template>
-  <footer class="w-full flex justify-center items-center bg-black text-white">
+  <footer class="w-full flex flex-col lg:flex-row justify-center items-center bg-black text-white">
     <a href="https://suckshop.it" target="_blank">
-    <img src="/images/logos/sucks-white.svg" alt="logo sucks white"/>
+      <img src="/images/logos/sucks-white.svg" alt="logo sucks white"/>
     </a>
 
     <a href="https://caracol-am.com" target="_blank">
-    <img src="/images/logos/designwanted-white.svg" alt="logo designwanted white"/>
+      <img src="/images/logos/designwanted-white.svg" alt="logo designwanted white"/>
     </a>
 
     <a href="https://designwanted.com" target="_blank">
-    <img src="/images/logos/caracol-white.png" alt="logo caracol white"/>
+      <img src="/images/logos/caracol-white.png" alt="logo caracol white"/>
     </a>
   </footer>
 </template>
 
 <style lang="postcss" scoped>
 footer {
-  padding: 70px 0;
+  padding: 70px 0 50px 0;
 
-  a:not(:first-child) {
-    margin-left: 70px;
+  @screen lg {
+    padding: 70px 0;
+  }
+
+  @screen lg {
+    a:not(:first-child) {
+      margin-left: 70px;
+    }
   }
 }
 
 img {
   max-height: 48px;
+  max-width: 174px;
   object-fit: contain;
   object-position: center;
+  margin-bottom: 20px;
+
+  @screen lg {
+    margin-bottom: 0;
+  }
+
+  @screen lg {
+    max-width: none;
+  }
 }
 </style>
