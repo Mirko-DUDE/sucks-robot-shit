67603b1c8b8de7929c660f1f1bac0db9aa7f6d34
Andrea Favaretto <andrea.favaretto@meduse.design>
2022-06-14 19:21:01 +0200

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -12,7 +12,7 @@
     </button>
 
     <div
-      class="absolute invisible opacity-0 flex justify-center items-center top-0 left-0 bg-black font-bold text-white text-24 uppercase leading-25 uppercase w-screen h-screen z-10"
+      class="absolute invisible opacity-0 flex justify-center items-center top-0 left-0 bg-black font-bold text-white text-24 lg:text-56 leading-35 lg:leading-64 uppercase w-screen h-screen z-10"
       ref="menu">
       <ul class="text-center">
         <li @click="toggleMenu">
